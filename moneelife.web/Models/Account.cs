1b4ba4b00e3b9c4397ed87e895aa349bf054f705
Варвара Фролова <radmila1604@gmail.com>
2018-03-03 20:30:25 +0100

    Snapshot model

---
+++ moneelife.web/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace moneelife.web.Models
+{
+    public class Account
+    {
+        public string Nickname { get; set; }
+
+        public decimal Balance { get; set; } 
+
+    }
+}
---
+++ moneelife.web/Models/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace moneelife.web.Models
+{
+    public class Expense
+    {
+    
+        /// <summary>
+        /// Total value of the mountly expenses 
+        /// </summary>
+        public decimal Total { get; set; }
+    }
+}
